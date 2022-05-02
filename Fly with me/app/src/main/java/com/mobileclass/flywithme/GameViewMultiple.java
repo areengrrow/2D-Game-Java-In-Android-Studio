d961575aa8f2747c7b902283cc635e840c18337d
TienTim <Tien.corbett@gmail.com>
2022-05-02 20:20:46 +0800

    Multiple connection

---
+++ Fly with me/app/src/main/java/com/mobileclass/flywithme/GameViewMultiple.java
@@ -0,0 +1,395 @@
+package com.mobileclass.flywithme;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mobileclass.flywithme.models.Post;
+import com.mobileclass.flywithme.models.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class GameViewMultiple extends SurfaceView implements Runnable {
+
+    private static final String TAG = "NewPost";
+    private static final String TAG_GET = "GetPost";
+    private Thread thread;
+    private boolean isPlaying, isGameOver = false;
+    private int screenX, screenY, score = 0;
+    public static float screenRatioX, screenRatioY;
+    private Paint paint;
+    private Bird[] birds;
+    private SharedPreferences prefs;
+    private Random random;
+    private SoundPool soundPool;
+    private List<Bullet> bullets;
+    private int sound;
+    private FlightMultiple flight;
+    private GameActivity activity;
+    private Background background1, background2;
+
+    private DatabaseReference mDatabase;
+    private DatabaseReference mPostReference;
+
+    public GameViewMultiple(GameActivity activity, int screenX, int screenY) {
+        super(activity);
+
+        this.activity = activity;
+
+        prefs = activity.getSharedPreferences("game", Context.MODE_PRIVATE);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_GAME)
+                    .build();
+
+            soundPool = new SoundPool.Builder()
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+
+        } else
+            soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+
+        sound = soundPool.load(activity, R.raw.shoot, 1);
+
+        this.screenX = screenX;
+        this.screenY = screenY;
+        screenRatioX = 1920f / screenX;
+        screenRatioY = 1080f / screenY;
+
+        background1 = new Background(screenX, screenY, getResources());
+        background2 = new Background(screenX, screenY, getResources());
+
+        flight = new FlightMultiple(this, screenY, getResources());
+
+        bullets = new ArrayList<>();
+
+        background2.x = screenX;
+
+        paint = new Paint();
+        paint.setTextSize(128);
+        paint.setColor(Color.WHITE);
+
+        birds = new Bird[4];
+
+        for (int i = 0;i < 4;i++) {
+
+            Bird bird = new Bird(getResources());
+            birds[i] = bird;
+
+        }
+
+        random = new Random();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        // Initialize Database
+        mPostReference = FirebaseDatabase.getInstance().getReference().child("posts");
+        addPostEventListener(mPostReference);
+
+    }
+
+
+    private void addPostEventListener(DatabaseReference mPostReference) {
+        // [START post_value_event_listener]
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                Post post = dataSnapshot.getValue(Post.class);
+                Log.w(TAG_GET, "post");
+                // ..
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG_GET, "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        mPostReference.addValueEventListener(postListener);
+        // [END post_value_event_listener]
+    }
+
+
+    @Override
+    public void run() {
+
+        while (isPlaying) {
+
+            update ();
+            draw ();
+            sleep ();
+
+        }
+
+    }
+
+    private void update () {
+
+        background1.x -= 10 * screenRatioX;
+        background2.x -= 10 * screenRatioX;
+
+        if (background1.x + background1.background.getWidth() < 0) {
+            background1.x = screenX;
+        }
+
+        if (background2.x + background2.background.getWidth() < 0) {
+            background2.x = screenX;
+        }
+
+        if (flight.isGoingUp)
+            flight.y -= 30 * screenRatioY;
+        else
+            flight.y += 30 * screenRatioY;
+
+        if (flight.y < 0)
+            flight.y = 0;
+
+        if (flight.y >= screenY - flight.height)
+            flight.y = screenY - flight.height;
+
+        List<Bullet> trash = new ArrayList<>();
+
+        for (Bullet bullet : bullets) {
+
+            if (bullet.x > screenX)
+                trash.add(bullet);
+
+            bullet.x += 50 * screenRatioX;
+
+            for (Bird bird : birds) {
+
+                if (Rect.intersects(bird.getCollisionShape(),
+                        bullet.getCollisionShape())) {
+
+                    score++;
+                    bird.x = -500;
+                    bullet.x = screenX + 500;
+                    bird.wasShot = true;
+
+                }
+
+            }
+
+        }
+
+        for (Bullet bullet : trash)
+            bullets.remove(bullet);
+
+        for (Bird bird : birds) {
+
+            bird.x -= bird.speed;
+
+            if (bird.x + bird.width < 0) {
+
+                if (!bird.wasShot) {
+                    isGameOver = true;
+                    return;
+                }
+
+                int bound = (int) (30 * screenRatioX);
+                bird.speed = random.nextInt(bound);
+
+                if (bird.speed < 10 * screenRatioX)
+                    bird.speed = (int) (10 * screenRatioX);
+
+                bird.x = screenX;
+                bird.y = random.nextInt(screenY - bird.height);
+
+                bird.wasShot = false;
+            }
+
+            if (Rect.intersects(bird.getCollisionShape(), flight.getCollisionShape())) {
+
+                isGameOver = true;
+                return;
+            }
+
+        }
+
+    }
+
+    private void draw () {
+
+        if (getHolder().getSurface().isValid()) {
+
+            Canvas canvas = getHolder().lockCanvas();
+            canvas.drawBitmap(background1.background, background1.x, background1.y, paint);
+            canvas.drawBitmap(background2.background, background2.x, background2.y, paint);
+
+            for (Bird bird : birds)
+                canvas.drawBitmap(bird.getBird(), bird.x, bird.y, paint);
+
+            canvas.drawText(score + "", screenX / 2f, 164, paint);
+
+            if (isGameOver) {
+                isPlaying = false;
+                canvas.drawBitmap(flight.getDead(), flight.x, flight.y, paint);
+                getHolder().unlockCanvasAndPost(canvas);
+                saveIfHighScore();
+                waitBeforeExiting ();
+                return;
+            }
+
+            canvas.drawBitmap(flight.getFlight(), flight.x, flight.y, paint);
+
+            for (Bullet bullet : bullets)
+                canvas.drawBitmap(bullet.bullet, bullet.x, bullet.y, paint);
+
+            getHolder().unlockCanvasAndPost(canvas);
+
+        }
+
+    }
+
+    private void waitBeforeExiting() {
+
+        try {
+            Thread.sleep(3000);
+            activity.startActivity(new Intent(activity, MainActivity.class));
+            activity.finish();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void saveIfHighScore() {
+
+        if (prefs.getInt("highscore", 0) < score) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("highscore", score);
+            editor.apply();
+        }
+
+    }
+
+    private void sleep () {
+        try {
+            Thread.sleep(17);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void resume () {
+
+        isPlaying = true;
+        thread = new Thread(this);
+        thread.start();
+
+    }
+
+    public void pause () {
+
+        try {
+            isPlaying = false;
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (event.getX() < screenX / 2) {
+                    flight.isGoingUp = true;
+                    flight.toShoot++;
+                }
+                final String userId = getUid();
+                mDatabase.child("users").child(userId).addListenerForSingleValueEvent(
+                        new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                // Get user value
+                                User user = dataSnapshot.getValue(User.class);
+
+                                if (user == null) {
+                                    // User is null, error out
+                                    Log.e(TAG, "User " + userId + " is unexpectedly null");
+                                    Toast.makeText(getContext(),
+                                            "Error: could not fetch user.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // Write new post
+                                    writeNewPost(userId, user.username, Integer.toString(score), "test");
+                                }
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                                Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                            }
+                        });
+                break;
+            case MotionEvent.ACTION_UP:
+                flight.isGoingUp = false;
+                if (event.getX() > screenX / 2)
+                    flight.toShoot++;
+                break;
+        }
+
+        return true;
+    }
+
+    public String getUid() {
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
+
+    private void writeNewPost(String userId, String username, String title, String body) {
+        // Create new post at /user-posts/$userid/$postid and at
+        // /posts/$postid simultaneously
+        String key = mDatabase.child("posts").push().getKey();
+        Post post = new Post(userId, username, title, body);
+        Map<String, Object> postValues = post.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/posts/" + key, postValues);
+        childUpdates.put("/user-posts/" + userId + "/" + key, postValues);
+
+        mDatabase.updateChildren(childUpdates);
+        Log.w(TAG, "post");
+    }
+
+    public void newBullet() {
+
+        if (!prefs.getBoolean("isMute", false))
+            soundPool.play(sound, 1, 1, 0, 0, 1);
+
+        Bullet bullet = new Bullet(getResources());
+        bullet.x = flight.x + flight.width;
+        bullet.y = flight.y + (flight.height / 2);
+        bullets.add(bullet);
+
+    }
+}
---
+++ Fly with me/app/src/main/java/com/mobileclass/flywithme/SignIn.java
@@ -0,0 +1,177 @@
+package com.mobileclass.flywithme;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.mobileclass.flywithme.models.User;
+
+public class SignIn extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "SignIn";
+    Button btnSignIn, btnSignUp;
+    EditText etEmail, etPassword;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSignIn = findViewById(R.id.buttonSignIn);
+        btnSignUp = findViewById(R.id.buttonSignUp);
+        btnSignIn.setOnClickListener(SignIn.this);
+        btnSignUp.setOnClickListener(SignIn.this);
+        etEmail = findViewById(R.id.fieldEmail);
+        etPassword = findViewById(R.id.fieldPassword);
+        mProgressBar = findViewById(R.id.progressBar);
+    }
+
+    private void signIn() {
+        Log.d(TAG, "signIn");
+//        if (!validateForm()) {
+//            return;
+//        }
+
+        showProgressBar();
+//        String email = etEmail.getText().toString();
+//        String password = etPassword.getText().toString();
+        String email = "a@mail.com";
+        String password = "123456";
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(SignIn.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signIn:onComplete:" + task.isSuccessful());
+                        hideProgressBar();
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(SignIn.this, "Sign In Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void signUp() {
+        Log.d(TAG, "signUp");
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressBar();
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(SignIn.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUser:onComplete:" + task.isSuccessful());
+                        hideProgressBar();
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(SignIn.this, "Sign Up Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void onAuthSuccess(FirebaseUser user) {
+        String username = usernameFromEmail(user.getEmail());
+
+        // Write new user
+        writeNewUser(user.getUid(), username, user.getEmail());
+
+        // Go to MainActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SignIn.this, MainActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+
+        mDatabase.child("users").child(userId).setValue(user);
+    }
+
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
+    }
+
+    private boolean validateForm() {
+        boolean result = true;
+        if (TextUtils.isEmpty(etEmail.getText().toString())) {
+            etEmail.setError("Required");
+            result = false;
+        } else {
+            etEmail.setError(null);
+        }
+
+        if (TextUtils.isEmpty(etPassword.getText().toString())) {
+            etPassword.setError("Required");
+            result = false;
+        } else {
+            etPassword.setError(null);
+        }
+
+        return result;
+    }
+
+    public void showProgressBar() {
+        if (mProgressBar != null) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void hideProgressBar() {
+        if (mProgressBar != null) {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        if (i == R.id.buttonSignIn) {
+            signIn();
+        } else if (i == R.id.buttonSignUp) {
+            signUp();
+        }
+    }
+}

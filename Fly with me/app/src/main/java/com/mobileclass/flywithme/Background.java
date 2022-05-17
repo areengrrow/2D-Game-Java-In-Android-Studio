9f7990305b1136431156e764293ab12d5c4e3993
dpong-vn <phong11a2002@gmail.com>
2022-05-17 19:43:57 +0800

    phong

--- Fly with me/app/src/main/java/com/mobileclass/flywithme/Background.java
+++ Fly with me/app/src/main/java/com/mobileclass/flywithme/Background.java
@@ -13,8 +13,7 @@ public class Background {
     Bitmap background;
 
     Background (int screenX, int screenY, Resources res) {
-        ImageView img = R.drawable.assassinscreed_back;
-        background = BitmapFactory.decodeResource(res, );
+        background = BitmapFactory.decodeResource(res, R.id.nature_back);
         background = Bitmap.createScaledBitmap(background, screenX, screenY, false);
 
     }

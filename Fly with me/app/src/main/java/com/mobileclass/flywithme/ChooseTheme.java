41f847f194df23a94acf1c6b49bd19a10af7e484
dpong-vn <phong11a2002@gmail.com>
2022-05-18 13:11:12 +0800

    Choose theme

--- Fly with me/app/src/main/java/com/mobileclass/flywithme/ChooseTheme.java
+++ Fly with me/app/src/main/java/com/mobileclass/flywithme/ChooseTheme.java
@@ -8,9 +8,10 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import com.mobileclass.flywithme.OpenClass;
 
 public class ChooseTheme extends AppCompatActivity {
-
+    OpenClass data = new OpenClass();
     ImageView back, next, assassin, fantasy, palace, nature;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +42,44 @@ public class ChooseTheme extends AppCompatActivity {
         assassin.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-
+                data.setTheme(1);
+                assassin.setImageResource(R.drawable.assassinscreed_back);
+                fantasy.setImageResource(R.drawable.fantasy_choose);
+                palace.setImageResource(R.drawable.palace_choose);
+                nature.setImageResource(R.drawable.nature_choose);
+                return false;
+            }
+        });
+        fantasy.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                data.setTheme(2);
+                assassin.setImageResource(R.drawable.assasin_choose);
+                fantasy.setImageResource(R.drawable.fantasyart_back);
+                palace.setImageResource(R.drawable.palace_choose);
+                nature.setImageResource(R.drawable.nature_choose);
+                return false;
+            }
+        });
+        palace.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                data.setTheme(3);
+                assassin.setImageResource(R.drawable.assasin_choose);
+                fantasy.setImageResource(R.drawable.fantasy_choose);
+                palace.setImageResource(R.drawable.palace_back);
+                nature.setImageResource(R.drawable.nature_choose);
+                return false;
+            }
+        });
+        nature.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                data.setTheme(4);
+                assassin.setImageResource(R.drawable.assasin_choose);
+                fantasy.setImageResource(R.drawable.fantasy_choose);
+                palace.setImageResource(R.drawable.palace_choose);
+                nature.setImageResource(R.drawable.nature_back);
                 return false;
             }
         });
--- Fly with me/app/src/main/java/com/mobileclass/flywithme/OpenClass.java
+++ Fly with me/app/src/main/java/com/mobileclass/flywithme/OpenClass.java
@@ -1,2 +1,20 @@
-package com.mobileclass.flywithme;public class OpenClass {
+//package com.mobileclass.flywithme;
+//public class OpenClass {
+//
+//}
+package com.mobileclass.flywithme;
+
+public class OpenClass {
+    public static int theme = 0;
+    public static long pages = 1053;
+    public void setTheme(int num)
+    {
+        theme = num;
+    }
+
+    public int getTheme()
+    {
+
+        return theme;
+    }
 }

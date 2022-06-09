0b6dfd7cd49129b0c056222ca2d3c8f707577357
areengrrow <trancanhthanh1@gmail.com>
2022-06-09 09:57:30 +0700

    update shaking

--- Fly with me/app/src/main/java/com/mobileclass/flywithme/Store.java
+++ Fly with me/app/src/main/java/com/mobileclass/flywithme/Store.java
@@ -6,17 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class Store extends AppCompatActivity {
     OpenClass data = new OpenClass();
-    ImageView back;
+    ImageView back, health, rocket;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
 
-        findViewById(R.id.btnBack).setOnTouchListener(new View.OnTouchListener() {
+        Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake_animation);
+        health = findViewById(R.id.healthImage);
+        rocket = findViewById(R.id.rocketImage);
+        back = findViewById(R.id.btnBack);
+
+        back.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 String place = data.get_place();
@@ -28,17 +36,19 @@ public class Store extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.healthImage).setOnTouchListener(new View.OnTouchListener() {
+        health.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
+                health.startAnimation(shake);
                 data.addHealth(1);
                 return false;
             }
         });
 
-        findViewById(R.id.rocketImage).setOnTouchListener(new View.OnTouchListener() {
+        rocket.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
+                rocket.startAnimation(shake);
                 data.addRocket(1);
                 return false;
             }
